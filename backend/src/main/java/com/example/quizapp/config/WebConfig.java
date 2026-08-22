1cbf63e5d516e86a43502b3f0e122a56448bb9d1
Shinya Sasaki <s_sasaki@spintechnology.js>
2026-08-22 19:27:32 +0900

    WebConfigを修正

--- backend/src/main/java/com/example/quizapp/config/WebConfig.java
+++ backend/src/main/java/com/example/quizapp/config/WebConfig.java
@@ -1,6 +1,7 @@
 package com.example.quizapp.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,5 +12,13 @@ public class WebConfig implements WebMvcConfigurer {
         // URLが /uploads/ で始まる場合、プロジェクト直下の uploads/ フォルダを見にいく
         registry.addResourceHandler("/uploads/**")
                 .addResourceLocations("file:uploads/");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://question-app-3rn.pages.dev") // あなたのCloudflare URL (末尾の/は無し)
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*");
     }
 }
