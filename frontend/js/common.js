b88d80e64d2b444b01175f7733d117c2ec86c7fb
Shinya Sasaki <s_sasaki@spintechnology.js>
2026-08-22 18:49:12 +0900

    ベースURL変更

--- frontend/js/common.js
+++ frontend/js/common.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:8080/api';
+const API_BASE_URL = 'https://question-app-3rn.pages.dev';
 
 function initTheme() {
       if (localStorage.getItem('theme') === 'dark') {
